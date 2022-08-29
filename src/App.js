6da37593ab76481ef428c31145bdc4b047a29348
mozammelhoq1 <mozammelhoq512@gmail.com>
2022-08-29 17:53:43 +0600

    navbar complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import NavigationBar from './Pages/Shared/NavigationBar/NavigationBar';
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
 
 function App() {
   return (
-    <div>
-      <button class="btn btn-primary">Button</button>
-    </div>
+    <>
+    <NavigationBar/>
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+      </>
   );
 }
 
---
+++ src/Pages/Shared/NavigationBar/NavigationBar.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import './NavigationBar.css';
+
+const NavigationBar = () => {
+  const menuItems = (
+    <>
+      <li className="menu-btn btn-design">
+        <Link to='/'>Home</Link>
+      </li>
+      <li className="menu-btn btn-design">
+        <Link to='portfolio'>Portfolio</Link>
+      </li>
+      <li className="menu-btn btn-design">
+        <Link to='about'>About</Link>
+      </li>
+      <li className="menu-btn btn-design">
+        <Link to='contact'>Contact</Link>
+      </li>
+    </>
+  );
+  return (
+    <div  className="navbar bg-primary text-primary-content ">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <label tabIndex="0" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </label>
+      <ul tabIndex="0" className="menu mockup-code  menu-compact dropdown-content mt-3 p-2 shadow bg-primary text-primary-content rounded-box w-52 ">
+        {menuItems}
+      </ul>
+    </div>
+    <a className="btn btn-ghost normal-case text-xl">Mozammel Hoq</a>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal p-0">
+      {menuItems}
+    </ul>
+  </div>
+  <div className="navbar-end">
+    <a className="btn btn-xs custom-btn btn-style">Subscribe</a>
+  </div>
+</div>
+  );
+};
+
+export default NavigationBar;
