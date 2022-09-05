bec7b140f31e127223b8a1fa7d81ed1354c754dc
mozammelhoq1 <mozammelhoq512@gmail.com>
2022-09-05 18:24:41 +0600

    banner complete

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,6 +1,7 @@
 import { transparent } from "daisyui/src/colors";
 import themes from "daisyui/src/colors/themes";
 import React, { useCallback } from "react";
+import AnimatedText from "react-animated-text-content";
 import Particles from "react-tsparticles";
 import Typical from "react-typical";
 import { loadFull } from "tsparticles";
@@ -9,125 +10,74 @@ import "./Banner.css";
 const Banner = () => {
   return (
     <>
-      <div className="container mx-auto flex">
-        <div className="flex-1">
-        <h3 className="text-center lg:text-left">Welcome to my portfolio</h3>
-        <h2>
-          Hi, I'm <span className="text-primary">MD. Mozammel Hoq</span>
-        </h2>
-        <span>
+      
+      <section className="container mx-auto">
+        <div class="min-h-screen">
+          <div class="hero-content flex-col lg:flex-row-reverse">
+            <img className=" md:max-w-sm rounded-full shadow-2xl hover:scale-75 duration-1000 hover:border hover:border-primary"
+              src={image} alt="profile"/>
+            <div>
+              <h1 class="text-4xl font-bold mb-4">
+              <span>
           Hello, I'M 🙋‍♂️{" "}
-          <span>
-            <Typical loop={Infinity} steps={["MD Mozammel Hoq", 2000]} />
+          
+          <span  className="text-primary ">
+            <Typical loop={Infinity} steps={["MD. Mozammel Hoq", 2000]} />
           </span>
         </span>
-        <h2>
+              </h1>
+              <h2 className="text-2xl font-semibold">
           <Typical
             loop={Infinity}
             steps={[
-              "Full Stack Dev  💻",
-              3000,
+              
               "Frontend Dev 🎯",
               3000,
               "MERN Stack Dev 😎",
               3000,
               "React Js Dev ⚛️",
               3000,
-              "React Native Dev 📱",
-              3000,
+              
             ]}
           />
         </h2>
-        <p>
-          I am a web developer from Dhaka, Bangladesh. My expertise is in
-          front-end web development. I know ReactJs, NodeJs, Express MongoDB,
-          etc. I have a beautiful taste in technology. I have done a full stack
-          web development course from programming hero to getting certified and
-          now I am ready to put myself in working sectors.
-        </p>
-
-        <a
-          href="https://drive.google.com/file/d/1yppXc_QMkVyvbgUj52f_cN3mEhzS3RVy/view?usp=sharing"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button
-            type="button"
-            className="mt-5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-8 py-3.5  dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-            data-mdb-ripple="true"
-            data-mdb-ripple-color="light"
+              <p className="py-6">
+              <AnimatedText
+            type="charts" // animate words or chars
+            animation={{
+              x: "200px",
+              y: "-20px",
+              scale: 1.1,
+              ease: "ease-in-out",
+            }}
+            animationType="wave"
+            interval={0.06}
+            duration={0.5}
+            tag="p"
+            className="animated-paragraph fw-bold font-monospace"
+            includeWhiteSpaces
+            threshold={0.1}
+            rootMargin="20%"
           >
-            Download Resume
-          </button>
-        </a>
-        </div>
-        <div className="flex-1">
-        <img className="cursor-pointer rounded-full " src={image} alt="" />
-        
-        </div>
-      </div>
-
-      {/* <div className='flex flex-wrap container'>
-          <div>
-            <h4 className="mt-16">Find Me Online:</h4>
-            <div className='mt-2'>
-              <a href="https://www.facebook.com/robinrakibul" target='_blank' rel="noreferrer">
-                <button className='btn_shadow p-2'>
-                  <i className='fab fa-facebook-f text-blue-500'></i>
-                </button>
-              </a>
-              <a href="https://www.facebook.com/robinrakibul" target='_blank' rel="noreferrer">
-                <button className='btn_shadow p-2'>
-                  <i className='fab fa-instagram text-orange-500'></i>
-                </button>
-              </a>
-              <a href="https://www.twitter.com/robinrakibul_" target='_blank' rel="noreferrer">
-                <button className='btn_shadow p-2'>
-                  <i className='fab fa-twitter text-orange-500'></i>
-                </button>
-              </a>
-              <a href="https://www.linkedin.com/in/robinrakibul" target='_blank' rel="noreferrer">
-                <button className='btn_shadow'>
-                  <i className='fab fa-linkedin text-blue-500'></i>
-                </button>
-              </a>
-              <a href="https://github.com/robinrakibul" target='_blank' rel="noreferrer">
-                <button className='btn_shadow'>
-                  <i className='fab fa-github text-black'></i>
-                </button>
-              </a>
-              
+            I'm a Front End Developer based in Bangladesh. I describe myself as
+            a passionate developer who loves coding, open source, and the web
+            platform. I like to create projects. That helps me to learn and grow
+            as a developer. Also I enjoy making technical things at my college
+            lab. In my free time you can find me tourist place ,at field , at
+            the road on cycling around Chittagong bangladesh. My future thinking
+            is that I will be a full fledged web developer, this is my
+            profession, passion and dream. I spend 8 to 12 hours a day trying to
+            reach my goals. And I am thinking of building a career in this field
+            in the future. Thank You ❤️
+          </AnimatedText>
+              </p>
+              <button class="btn btn-primary btn-sm hover:bg-transparent hover:text-primary font-bold ">Get Started</button>
             </div>
           </div>
-        </div> */}
-
-      {/* <div className="hero-content text-center text-light-content">
-        <div className="max-w-md">
-          <span>
-            Hello, I'M 🙋‍♂️{" "}
-            <span>
-              <Typical loop={Infinity} steps={["MD Mozammel Hoq", 2000]} />
-            </span>
-          </span>
-          <h2>
-            <Typical
-              loop={Infinity}
-              steps={[
-                "Full Stack Dev  💻",
-                3000,
-                "Frontend Dev 🎯",
-                3000,
-                "MERN Stack Dev 😎",
-                3000,
-                "React Js Dev ⚛️",
-                3000,
-                "React Native Dev 📱",
-                3000,
-              ]}
-            />
-          </h2>
         </div>
-      </div> */}
+      </section>
+
+      
     </>
   );
 };
